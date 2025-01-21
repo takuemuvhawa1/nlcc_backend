aae47eb306b6d12f5a901249fd2a0ba5e73d7bdb
taku1muvhawa <taku1muvhawa@gmail.com>
2025-01-21 16:13:51 +0200

    21-1-2025

--- cruds/poolapi.js
+++ cruds/poolapi.js
@@ -1,4 +1,5 @@
 // const pool = 'http://localhost:3003';
-const pool = 'https://nlcc-backend-1.onrender.com';
+// const pool = 'https://nlcc-backend-1.onrender.com';
+const pool = 'https://nlcc-backend-4t7c.onrender.com';
 
 module.exports = pool; 
